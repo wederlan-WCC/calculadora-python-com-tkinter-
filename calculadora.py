de6ea2e18335e814fd7fc0246e99f32ab18b3382
wederlan-WCC <71ctpm2025@gmail.com>
2026-01-21 20:10:08 -0300

    cometando no codigo calculadora.py
    agora o código ta bem comentadinho

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from sympy import sympify
-
+#cria uma janela
 j = Tk()
+#configura a cor da janela para cinza
 j.config(bg="#2F2F2F")
+#cria o valor do display
 value=""
+#cria o disaplay e o configura
 display = Label(
     j,
     text="",
@@ -12,19 +15,20 @@ display = Label(
     width=30
 )
 display.place(x=110,y=15)
-
+#cria uma class botão sem si inporta com carmelcase
 class bot:
     def __init__(self,num,ro,co):
         self.num = num
         self.botn = Button(j, text=str(num),command=self.clik)
         self.botn.config(bg="#341539",fg="#9F9F9F")
         self.botn.place(x=int(ro),y=int(co))
+    #cria uma funsão para adisionar o botão no display
     def clik(self):
         global value
         global display
         value=value+str(self.num)
         display.config(text=value)
-        
+#cria uma funsão para pegar o resultado comsympify em vez de eval por não dar bugs com a base numerica octal
 def eq():
     global value
     value2=value.replace("x","*")
@@ -32,14 +36,17 @@ def eq():
     value=float(sympify(value3))
     value=str(value)
     display.config(text=str(value))
+#cria uma função para limpa o display
 def clr():
     global value
     value=""
     display.config(text=value)
+#outra para limpar apenas um numero
 def clrear():
     global value
     value=str(value)[:-1]
     display.config(text=value)
+#cria todos os botoes
 bc=Button(j,text="C",command=clr)
 bex=Button(j,text="\u2190",bg="#341539",fg="#9F9F9F",command=clrear)
 bex.place(x=400,y=70)
@@ -63,5 +70,5 @@ bmas=bot("+",500,390)
 equal=Button(j,text="=",command=eq)
 equal.config(bg="#341539",fg="#9F9F9F")
 equal.place(x=400,y=390)
-
+#usa-se mainloop para a janela não feichar
 j.mainloop()
